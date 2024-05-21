e0de3ac87a67d50fbf5f96d5e195cc600e642aca
John Jones <johnbrianjones@pm.me>
2024-05-21 01:15:58 -0700

    add decoder tests

--- src/decoder/tests.rs
+++ src/decoder/tests.rs
@@ -1,23 +1,72 @@
 use super::*;
 
 #[test]
-fn encode_bifid() {
+fn decode_bifid() {
     let pk = PolymorphicKey::new(
         String::from("MEOW"),
         String::from("ABCDEFGHIKLMNOPQRSTUVWXYZ"),
         Vec::from([5, 5]),
     );
 
-    let output = String::from("./tmp/test-encoder-bifid-out.txt");
-    let encoder = Encoder::new(
-        String::from("./fixtures/encoder-basic-in.txt"),
+    let output = String::from("./tmp/test-decoder-bifid-out.txt");
+    let decoder = Decoder::new(
+        String::from("./fixtures/test-decoder-bifid-in.txt"),
         output.clone(),
         pk,
         0,
+        String::from(""),
     );
 
-    encoder.encode();
+    decoder.decode();
 
     let res = fs::read_to_string(output).expect("failed to read output");
-    assert_eq!(res, "MMEL");
+    assert_eq!(res, "MEOW");
+}
+
+#[test]
+fn decode_trifid() {
+    let pk = PolymorphicKey::new(
+        String::from("MEOW"),
+        String::from("ABCDEFGHIJKLMNOPQRSTUVWXYZ+"),
+        Vec::from([3, 3, 3]),
+    );
+
+    let output = String::from("./tmp/test-decoder-trifid-out.txt");
+    let decoder = Decoder::new(
+        String::from("./fixtures/test-decoder-trifid-in.txt"),
+        output.clone(),
+        pk,
+        0,
+        String::from(""),
+    );
+
+    decoder.decode();
+
+    let res = fs::read_to_string(output).expect("failed to read output");
+    assert_eq!(res, "MEOW");
+}
+
+#[test]
+fn encode_big() {
+    let pk = PolymorphicKey::new(
+        String::from(""),
+        String::from("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"),
+        Vec::from([4, 4, 4]),
+    );
+
+    let output = String::from("./tmp/test-decoder-big-out.txt");
+    let decoder = Decoder::new(
+        String::from("./fixtures/test-decoder-big-in.txt"),
+        output.clone(),
+        pk,
+        0,
+        String::from(""),
+    );
+
+    decoder.decode();
+
+    let expected = fs::read_to_string("./fixtures/expected-encoder-big.txt")
+        .expect("failed to load expected output");
+    let res = fs::read_to_string(output).expect("failed to read output");
+    assert_eq!(res, expected);
 }
